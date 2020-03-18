635feb5b3c50733b76004ce627ae7881b447a944
David Márquez <davidmarquezing@gmail.com>
2020-03-17 19:01:00 -0500

    Movimiento del corredor numero 2

--- src/main/java/edu/unicundi/carrerarelevos/Corredor.java
+++ src/main/java/edu/unicundi/carrerarelevos/Corredor.java
@@ -19,11 +19,15 @@ public class Corredor extends Thread {
     private Equipo equipo;
     private int auxiliar;
     private int recorrido;
+    private int recorrido2;
+    private int contador;
 
     public Corredor(String figura, int posicion, Equipo equipo) {
         this.figura = figura;
         this.posicion = posicion;
         this.equipo = equipo;
+        this.recorrido2 = 30;
+        this.contador = 0;
 
         for (int i = 0; i < this.equipo.getDistancia().length; i++) {
             if (i == 0 && this.posicion == 1) {
@@ -41,42 +45,90 @@ public class Corredor extends Thread {
 
     public void run() {
         correr();
-        
+
     }
 
-    public void correr() {      
-        this.auxiliar = this.recorrido;
-        this.recorrido += (int) Math.floor(Math.random() * 4 + 1);
+    public void correr() {
+
         try {
-            Thread.sleep(1000);            
+            Thread.sleep(1000);
         } catch (InterruptedException ex) {
             Logger.getLogger(Corredor.class.getName()).log(Level.SEVERE, null, ex);
         }
         if (this.posicion == 1) {
             this.equipo.getDistancia()[this.auxiliar] = "-";
             this.equipo.getDistancia()[this.recorrido] = this.figura;
-            if (this.recorrido <= 30) {
+            if (this.recorrido != 30) {
+
+                if (this.recorrido < 30) {
+                    this.auxiliar = this.recorrido;
+                    this.recorrido += (int) Math.floor(Math.random() * 4 + 1);
+                    if (this.recorrido > 30) {
+                        this.auxiliar = this.recorrido;
+                        this.recorrido = 30;
+                    }
+                }
                 correr();
-            }else{
-                equipo.setEstado(true);
-            }
+            } 
+//            else {
+////                this.equipo.getDistancia()[this.auxiliar] = "-";
+////                this.equipo.getDistancia()[this.recorrido] = this.figura;
+//                equipo.setEstado(true);
+//            }
             synchronized (equipo) {
                 equipo.notifyAll();
             }
-        } else {
-            synchronized (equipo) {
-//                System.out.println("Corredor " + this.figura + " en espera");
-//                System.out.println("");
-                try {
-                    equipo.wait();
-                } catch (InterruptedException ex) {
-                    Logger.getLogger(Corredor.class.getName()).log(Level.SEVERE, null, ex);
+        } else if (this.posicion == 2) {
+            equipo.setEstado(false);
+            this.equipo.getDistancia()[this.auxiliar] = "-";
+            this.equipo.getDistancia()[30]="A";
+            this.equipo.getDistancia()[this.recorrido2] = this.figura;
+            if (contador == 0) {
+
+                synchronized (equipo) {
+                    try {
+                        equipo.wait();
+                    } catch (InterruptedException ex) {
+                        Logger.getLogger(Corredor.class.getName()).log(Level.SEVERE, null, ex);
+                    }
+                }
+
+                if (this.recorrido2 != 60) {
+                    if (this.recorrido2 < 60) {
+
+                        this.auxiliar = this.recorrido2;
+                        this.recorrido2 += (int) Math.floor(Math.random() * 4 + 1);
+                        if (this.recorrido2 > 60) {
+                            this.auxiliar = this.recorrido2;
+                            this.recorrido2 = 60;
+                        }
+                    }
+                    contador = 1;
+                    correr();
+                } else {
+                    equipo.setEstado(true);
+                }
+            } else {
+
+                if (this.recorrido2 != 60) {
+                    if (this.recorrido2 < 60) {
+
+                        this.auxiliar = this.recorrido2;
+                        this.recorrido2 += (int) Math.floor(Math.random() * 4 + 1);
+
+                        if (this.recorrido2 > 60) {
+                            this.auxiliar = this.recorrido2;
+                            this.recorrido2 = 60;
+                        }
+                    }
+                    contador = 1;
+                    correr();
+                } else {
+                    equipo.setEstado(true);
                 }
             }
-//            System.out.println("Corredor " + this.figura + " empiezo a correr");
-//            System.out.println("");
+
         }
-        
+
     }
-    
 }
